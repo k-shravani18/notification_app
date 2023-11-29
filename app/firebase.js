4cccadcabe78110c5d40856bbd3de6c545442cb5
k-shravani18 <karupakalashravani.com>
2023-11-29 12:17:46 +0530

    second commit

---
+++ app/firebase.js
@@ -0,0 +1,48 @@
+"use client";
+import React from "react";
+import { initializeApp } from "firebase/app";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD23nyKizSD4vUnH1TVBe-TqYt0UCscixc",
+  authDomain: "notification-99798.firebaseapp.com",
+  projectId: "notification-99798",
+  storageBucket: "notification-99798.appspot.com",
+  messagingSenderId: "903903576370",
+  appId: "1:903903576370:web:06b364114070f6bae1172b",
+  measurementId: "G-HJNMD889H3",
+};
+initializeApp(firebaseConfig);
+const messaging = getMessaging();
+
+export const requestForToken = async () => {
+  try {
+    const messagingResolve = messaging;
+    const currentToken = await getToken(messagingResolve, {
+      vapidKey:
+        "BBv0Z3XuuPAH8ef_LQHc8iBjkBwYD2d-dC7SK7PHrbzOUHt5fmDmuHQaE-5nUZWPfjACF3hjy9LvVZJG1mixb4o",
+    });
+    if (currentToken) {
+      console.log("current token for client: ", currentToken);
+    } else {
+      // Show permission request UI
+      console.log(
+        "No registration token available. Request permission to generate one."
+      );
+    }
+  } catch (err) {
+    console.log("An error occurred while retrieving token. ", err);
+  }
+};
+
+export const onMessageListener = async () =>
+  new Promise((resolve) =>
+    (async () => {
+      const messagingResolve = messaging;
+      onMessage(messagingResolve, (payload) => {
+        console.log("payload", payload);
+        resolve(payload);
+      });
+    })()
+  );
---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,30 @@
+// Scripts for firebase and firebase messaging
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
+
+// Initialize the Firebase app in the service worker by passing the generated config
+const firebaseConfig = {
+  apiKey: "AIzaSyD23nyKizSD4vUnH1TVBe-TqYt0UCscixc",
+  authDomain: "notification-99798.firebaseapp.com",
+  projectId: "notification-99798",
+  storageBucket: "notification-99798.appspot.com",
+  messagingSenderId: "903903576370",
+  appId: "1:903903576370:web:06b364114070f6bae1172b",
+  measurementId: "G-HJNMD889H3",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function (payload) {
+  console.log("Received background message ", payload);
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
